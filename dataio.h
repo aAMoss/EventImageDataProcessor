75b9038acffe8cf609b95e8f70287c089615de01
aAMoss <92592883+aAMoss@users.noreply.github.com>
2022-05-13 10:43:09 +0100

    Output file function working

--- dataio.h
+++ dataio.h
@@ -54,13 +54,14 @@ void dataio_set_event_packet_vars(int *packet_size, int *packet_overlap);
 
 DIR *dataio_open_data_input_dir(DIR *Data_Input_Dir, int c);
 FILE *dataio_open_data_input_file(DIR *Data_Input_Dir, FILE *Sample_Input_File, int c );
-
 DIR *dataio_open_data_output_dir_test(DIR *Data_Output_Dir, char* output_dir_name, int c);
 FILE *dataio_open_data_output_file(DIR *Data_Output_Dir, char* output_dir_name, FILE *Processed_Data_Output_File, int c);
+
 
 void dataio_get_input_sample_var(FILE *Sample_Input_File, long int *sample_bytes, long int *sample_events);
 void datio_set_secondary_event_packet_vars(long int sample_events, int packet_size, int packet_overlap, int *packets_req,
                                            int *packet_events_overshoot, int *last_packet_zeros, int *last_packet_size);
+
 
 void dataio_zero_event_packet_arrays(long int EventPacketX[],long int EventPacketY[],long int EventPacketP[],long int EventPacketT[]);
 void dataio_extract_event_packets(FILE *Sample_Input_File, int byte_no, int f_packet_size, int *packet_event_no,long int EventPacketX[], long int EventPacketY[], long int EventPacketP[],long int EventPacketT[]);
