69d7a212d00ac63bf6f7cbfba8b2a50a8c66a72f
aAMoss <92592883+aAMoss@users.noreply.github.com>
2022-05-14 17:37:18 +0100

    basic parameters for tdma create

--- tmda.c
+++ tmda.c
@@ -17,14 +17,67 @@
 #include "tmda.h"
 
 
+
+int test_samples;
+int train_samples;
+
+void tmda_set_data_samples(int *test_samples, int *train_samples);
+
 int main(void)
 {
     
     
+    tmda_set_data_samples(&test_samples, &train_samples);
     
-    
-    
+    printf("Selected:\t%d Test Samples\t%d Training Samples\n", test_samples, train_samples);
     
     
     return 0;
+}
+
+
+
+
+void tmda_set_data_samples(int *test_samples, int *train_samples)
+{
+    int test = 0;
+    int train = 0;
+    int test_flag = 0;
+    int train_flag = 0;
+    
+    while(test_flag < 1)
+    {
+        printf("Please enter the required number of test samples.\nValue must be at least 1.\n");
+       
+        scanf("%d", &test);
+        
+        if(test < 1)
+        {
+            printf("ERROR: Must have at least 1 sample!\n");
+        }
+        else
+        if (test >= 1)
+        {
+            *test_samples = test;
+            test_flag++;
+        }
+    }
+    
+    while(train_flag < 1)
+    {
+        printf("Please enter the required number of training samples.\nValue must be at least 1.\n");
+       
+        scanf("%d", &train);
+        
+        if(train < 1)
+        {
+            printf("ERROR: Must have at least 1 sample!\n");
+        }
+        else
+        if (train >= 1)
+        {
+            *train_samples = train;
+            train_flag++;
+        }
+    }
 }
