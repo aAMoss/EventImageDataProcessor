6c5ae5e93e39c67b047e78b47eaf59860c1bd866
aAMoss <92592883+aAMoss@users.noreply.github.com>
2022-05-14 19:02:35 +0100

    updated .gitignore, function to select dataset name finished

--- tmda.c
+++ tmda.c
@@ -16,32 +16,56 @@
 // Program Specific Headers
 #include "tmda.h"
 
+// Definitions
+#define TEST_DATA_NAME_MAX 200
 
 
-int test_samples;
-int train_samples;
+// Variables
 
-void tmda_set_data_samples(int *test_samples, int *train_samples);
+long int test_samples;
+long int train_samples;
+char test_dir_label[(TEST_DATA_NAME_MAX /2)];
+
+
+
+// Function Prototypes
+void tmda_get_test_dir_label(char *test_dir_label);
+void tmda_set_data_samples(long int *test_samples, long int *train_samples);
+
 
 int main(void)
 {
     
+    tmda_get_test_dir_label(test_dir_label);
     
     tmda_set_data_samples(&test_samples, &train_samples);
     
-    printf("Selected:\t%d Test Samples\t%d Training Samples\n", test_samples, train_samples);
+    printf("Data Directory Name:\t%s\n", test_dir_label);
+    printf("Selected:\t%ld Test Samples\t%ld Training Samples\n", test_samples, train_samples);
+    
     
     
     return 0;
 }
 
-
-
-
-void tmda_set_data_samples(int *test_samples, int *train_samples)
+void tmda_get_test_dir_label(char *test_dir_label)
 {
-    int test = 0;
-    int train = 0;
+    char user_input[TEST_DATA_NAME_MAX];
+
+    
+    puts("Please enter the name of the data test data directory.");
+
+    fgets(user_input, (TEST_DATA_NAME_MAX), stdin);
+    user_input[strcspn(user_input, "\n")] = '\0'; // removes the return key
+    strcat(test_dir_label, user_input);
+}
+
+
+
+void tmda_set_data_samples(long int *test_samples, long int *train_samples)
+{
+    long int test = 0;
+    long int train = 0;
     int test_flag = 0;
     int train_flag = 0;
     
@@ -49,7 +73,7 @@ void tmda_set_data_samples(int *test_samples, int *train_samples)
     {
         printf("Please enter the required number of test samples.\nValue must be at least 1.\n");
        
-        scanf("%d", &test);
+        scanf("%ld", &test);
         
         if(test < 1)
         {
@@ -67,7 +91,7 @@ void tmda_set_data_samples(int *test_samples, int *train_samples)
     {
         printf("Please enter the required number of training samples.\nValue must be at least 1.\n");
        
-        scanf("%d", &train);
+        scanf("%ld", &train);
         
         if(train < 1)
         {
@@ -81,3 +105,5 @@ void tmda_set_data_samples(int *test_samples, int *train_samples)
         }
     }
 }
+
+
