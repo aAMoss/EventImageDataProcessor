13aea379ff18bcbb1c0490b99bc89c895e8208b2
aAMoss <92592883+aAMoss@users.noreply.github.com>
2022-05-15 19:57:22 +0100

    got the gist of how the shuffle program will work, will write the functions to open the relevant io files and the copy function next

--- tmda.c
+++ tmda.c
@@ -33,8 +33,27 @@
 // going to need a function to copy the contents of the dataset file to the appropriate test and train files... basically concatenating them together.
 
 
+// will first count the number of files in the directory and record the entries
+// will then rewind to the directory start
+//  will randomly step through the directory selecting samples, will record the name of selected samples
+// willuse the recorded name to ensure duplicates of samples aren't selected
+
+#define MIN_CLASS 0
+#define MAX_CLASS 9
+#define MIN_POS 0
+#define SAMPLE_STAT 1
 
 
+
+
+int copied_test_samples;
+int copied_train_samples;
+
+int test_class_sample_count[CLASSES] = {0};
+int train_class_sample_count[CLASSES] = {0};
+
+int test_class_status[CLASSES];
+int train_class_status[CLASSES];
 
 
 
@@ -43,6 +62,8 @@
 int main(void)
 {
     
+    srand(time(NULL));
+    
     tmda_get_dataset_dir_label(dataset_dir_label);
     
     tmda_set_data_samples(&test_samples, &train_samples);
@@ -50,64 +71,124 @@ int main(void)
     tmda_get_data_sample_per_class(test_samples, train_samples, &class_test_samples, &class_train_samples,
                                    &total_test_samples, &total_train_samples, &total_nmnist_samples);
     
-    // Going to create / open the output files here
     
-    // Selecting Test Data
+    // Counts the number of samples in each class for the test and train data
     for(int c = 0; c < CLASSES; c++)
     {
-        
         DATASET_INPUT_DIR = tmda_open_dataset_input_dir_test(DATASET_INPUT_DIR, dataset_dir_label, c);
+        rewinddir(DATASET_INPUT_DIR);
         
-        
-        // will first count the number of files in the directory and record the entries
-        // will then rewind to the directory start
-        //  will randomly step through the directory selecting samples, will record the name of selected samples
-        // willuse the recorded name to ensure duplicates of samples aren't selected
-        
-        
-        
-        
-        // going to open  the input files here
-        
-        //
-        
-        //close file here
+        while(  (Dataset_Input_Dir_Entry = readdir(DATASET_INPUT_DIR)) )
+        {
+            if(strcmp(Dataset_Input_Dir_Entry->d_name, ".") != 0 && strcmp(Dataset_Input_Dir_Entry->d_name, "..") != 0 &&
+               strcmp(Dataset_Input_Dir_Entry->d_name, ".DS_Store"))
+            {
+                test_class_sample_count[c]++;
+            }
+        }
         closedir(DATASET_INPUT_DIR);
         
-        
-    }
-    
-    // Selecting Training Data
-    for(int c = 0; c < CLASSES; c++)
-    {
         
         DATASET_INPUT_DIR = tmda_open_dataset_input_dir_train(DATASET_INPUT_DIR, dataset_dir_label, c);
+        rewinddir(DATASET_INPUT_DIR);
         
-        
+        while(  (Dataset_Input_Dir_Entry = readdir(DATASET_INPUT_DIR)) )
+        {
+            if(strcmp(Dataset_Input_Dir_Entry->d_name, ".") != 0 && strcmp(Dataset_Input_Dir_Entry->d_name, "..") != 0 &&
+               strcmp(Dataset_Input_Dir_Entry->d_name, ".DS_Store"))
+            {
+                train_class_sample_count[c]++;
+            }
+        }
         closedir(DATASET_INPUT_DIR);
-        
-        
     }
     
-    
-    
+    printf("Class\tTest\tTrain\n");
+    printf("No\tSamples\tSamples\n");
+    for(int i = 0; i < CLASSES; i++)
+    {
+        printf("%d\t%d\t%d\n", i, test_class_sample_count[i], train_class_sample_count[i]);
+        
+    }
     
     printf("Data Directory Name:\t%s\n", dataset_dir_label);
     printf("Test Samples: %ld\nTraining Samples: %ld\n", test_samples, train_samples);
     printf("Samples per test class: %ld\nSamples per train class: %ld\n", class_test_samples, class_train_samples);
     printf("Total test samples: %ld\nTotal train samples: %ld\n", total_test_samples, total_train_samples);
     printf("Total nmnist samples: %ld\n", total_nmnist_samples);
+  
     
     
-    //function to open the directory... recursively maybe a loop similar to the main program
-    
-    // function to open the sample file
-    
-    // function to copy/append the sample file to the output file
-    
-    // rinse and repeat for the number of samples required
+    printf("random_class\ttest_pos\ttrain_pos\n");
     
     
+    
+    
+    for(int i = 0; i < CLASSES; i++)
+    {
+        
+        
+    }
+    
+    
+    copied_test_samples = 0;
+    
+    
+    
+    
+    
+    while(copied_test_samples < class_test_samples)
+    {
+        
+        int random_class = ( rand() % (MAX_CLASS - MIN_CLASS + 1) ) + MIN_CLASS;
+        
+        int local_class_sample_count = test_class_sample_count[random_class];
+        
+        long int dir_pos = ( rand() % (test_class_sample_count[random_class] - MIN_POS + 1) ) + MIN_POS;
+        
+        int class_status[ (test_class_sample_count[random_class]) ];
+
+        
+        DATASET_INPUT_DIR = tmda_open_dataset_input_dir_test(DATASET_INPUT_DIR, dataset_dir_label, random_class);
+
+        seekdir(DATASET_INPUT_DIR, dir_pos);
+        
+        while(  (Dataset_Input_Dir_Entry = readdir(DATASET_INPUT_DIR)) )
+        {
+           if(strcmp(Dataset_Input_Dir_Entry->d_name, ".") != 0 && strcmp(Dataset_Input_Dir_Entry->d_name, "..") != 0 &&
+              strcmp(Dataset_Input_Dir_Entry->d_name, ".DS_Store"))
+           {
+               
+               for(int i = 0; i < test_class_sample_count[random_class]; i ++)
+               {
+                   
+                   if( class_status[i] == 0 )
+                   {
+                       
+                       //open input sample file
+                       //copy input sample contents to the output file
+                       
+                       class_status[i]++;
+                       copied_test_samples++;
+                       break;
+                   }
+                   
+                   break;
+                   
+               }
+
+           }
+            
+        }
+        
+  
+        
+      
+
+    }
+    
+    
+
     return 0;
 }
 
