ffa434790b87693e5f5b076b7158893b9533d72f
aAMoss <92592883+aAMoss@users.noreply.github.com>
2022-06-12 22:10:24 +0100

    making progress on commandline args

--- eidp.c
+++ eidp.c
@@ -57,6 +57,8 @@ void eidp_print_log_file1_train(FILE *EIDP_LOG_FILE_TRAIN, int class,
 
 void parse_cmd_args(int argc, char *argv[])
 {
+
+
 	// Check number of arguments
 	if( argc == 5 )
 	{
@@ -70,10 +72,14 @@ void parse_cmd_args(int argc, char *argv[])
 			printf("Too few arguments, five expected. Program terminating.\n");
 			exit(EXIT_FAILURE);
 	}
+
+
 }
 
 void parse_cmd_args_method(int argc, char *argv[], int *fe_mode)
 {
+
+
 	// Parse argv[1] the feature extraction method
 	if(strcmp(argv[1], "raw") == 0)
 	{
@@ -94,10 +100,14 @@ void parse_cmd_args_method(int argc, char *argv[], int *fe_mode)
 		printf("Feature extraction method selection error. Program terminating.\n");
 		exit(EXIT_FAILURE);
 	}
+
+
 }
 
 void parse_cmd_args_features(int argc, char *argv[], int fe_mode, int *features)
 {
+
+
 	// Parse argv[2] the number of features (a.k.a inputs for the Tsetlin Machine)
 	int f = 0;
 	int flag = 0;
@@ -165,12 +175,60 @@ void parse_cmd_args_features(int argc, char *argv[], int fe_mode, int *features)
 	}
 
 
-	printf("Features %d\n", *features);
+}
+
+void parse_cmd_args_packet_size(int argc, char *argv[], int *packet_size)
+{
+	int p = 0;
+	int flag = 0;
+
+	p = atoi(argv[3]);
+
+	if(p >= PACKET_MIN && p <= PACKET_MAX)
+	{
+		*packet_size = p;
+	} else
+	{
+		while(flag < 1)
+		{
+			printf("\nEnter an Event Packet Size in the range %d to %d.\n\n", PACKET_MIN, PACKET_MAX);
+
+			scanf("%d", &p);
+
+			if(p < PACKET_MIN || p > PACKET_MAX)
+			{
+				printf("\nERROR: Event Packet Size must be between %d and %d!\n\n", PACKET_MIN, PACKET_MAX);
+			}
+			else
+			if(p >= PACKET_MIN && p <= PACKET_MAX)
+			{
+				*packet_size = p;
+				flag++;
+			}
+		} // end while
+	} // end if
+
+
+}
+
+void parse_cmd_args_packet_overlap(int argc, char *argv[], int packet_size, int *packet_overlap)
+{
+	int p = 0;
+	int flag = 0;
+
+	p = atoi(argv[4]);
+
 
 }
 
 
+void parse_cmd_args_out_dir_label(int argc, char *argv[], int fe_mode, int features, int packet_size, int packet_overlap, char *output_dir_label)
+{
 
+
+
+
+}
 
 int main(int argc, char *argv[])
 {
@@ -178,9 +236,9 @@ int main(int argc, char *argv[])
 	parse_cmd_args(argc, argv);
 	parse_cmd_args_method(argc, argv, &fe_mode);
 	parse_cmd_args_features(argc, argv, fe_mode, &features);
-//	parse_cmd_args_packet_size(argc, argv, &fe_mode, &features, &packet_size, &packet_overlap, output_dir_label);
-//	parse_cmd_args_packet_overlap(argc, argv, &fe_mode, &features, &packet_size, &packet_overlap, output_dir_label);
-//	parse_cmd_args_out_dir_label(argc, argv, &fe_mode, &features, &packet_size, &packet_overlap, output_dir_label);
+	parse_cmd_args_packet_size(argc, argv, &packet_size);
+	parse_cmd_args_packet_overlap(argc, argv, packet_size, &packet_overlap);
+	parse_cmd_args_out_dir_label(argc, argv, fe_mode, features, packet_size, packet_overlap, output_dir_label);
 
     clock_t start, end;
     double cpu_time_used;
