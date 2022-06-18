81bb778406d10136e8446d8e4de5f9f99ec70657
aAMoss <92592883+aAMoss@users.noreply.github.com>
2022-06-18 22:45:37 +0100

    headers in config.c

--- config.c
+++ config.c
@@ -1,1 +1,20 @@
 // config.c - Configuration Parameters
+
+
+// Standard Headers
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <math.h>
+
+//Posix Headers
+#include <dirent.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+// Program Specific Headers
+
+
+
